85025f9e89bf0e646636088de0c5de391f1a7f26
Mustafa Özdemir <mustafa.ozedmir1408@gmail.com>
2025-07-13 22:36:29 +0200

    Introduction to Migrations

---
+++ database/migrations/2025_07_13_203101_create_jobs_listing.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs_listing', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('company')->nullable();
+            $table->string('salary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs_listing');
+    }
+};
