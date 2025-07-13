67131deccb5a9157d643ccff5dc68efa923cdf2f
Mustafa Özdemir <mustafa.ozedmir1408@gmail.com>
2025-07-13 14:58:23 +0200

    View Data and Route Wildcards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,15 @@
 <h1>Welcome to the Home Page</h1>
 <p>This is the home page of the Laravel application.</p>
 <p>Here you can find links to other pages like About and Contact.</p>
+<div class="mt-6">
+    <h2 class="text-xl font-semibold">Available Jobs</h2>
+    <ul class="mt-4 space-y-2">
+        @foreach ($jobs as $job)
+            <li class="p-4 bg-white shadow rounded-md">
+                <h3 class="text-lg font-medium">{{ $job['title'] }}</h3>
+                <p class="text-gray-600">Company: {{ $job['company'] }}</p>
+            </li>
+        @endforeach
+    </ul>
+</div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $jobs=[
+        ['title' => 'Software Engineer', 'company' => 'Tech Corp'],
+        ['title' => 'Data Analyst', 'company' => 'Data Solutions'],
+        ['title' => 'Web Developer', 'company' => 'Web Innovations'],
+    ];
+    return view('home', ['jobs' => $jobs]);
+
 });
 
 
